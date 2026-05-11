b3de0fe37c44ecca9760949550dd591c381b163a
Mel Adry <meladryyy@gmail.com>
2026-05-11 19:46:02 +0800

    try rebuild (..)

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,8 +14,5 @@ export default defineConfig({
         resolve: {
             alias: { "@": "/src", },
         },
-        define: {
-            "process.env": process.env
-        }
     },
 });
