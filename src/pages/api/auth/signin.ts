0fefc79bb752fea8176ea75eee86c310a495a111
Mel Adry <meladryyy@gmail.com>
2026-05-13 22:13:07 +0800

    fixed

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,8 +1,9 @@
+export const prerender = false
+
 import type { APIRoute } from "astro";
 import { supabase } from "@/lib/supabase";
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
-
   const formData = await request.formData();
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
